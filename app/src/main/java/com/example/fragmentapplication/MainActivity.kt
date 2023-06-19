6e84b4e7f33f26f04eadb5b766ef19bec4cf244c
surbhi150 <135599566+surbhi150@users.noreply.github.com>
2023-06-20 01:24:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragmentapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.fragmentapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Activity Created", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    class MainActivity : AppCompatActivity() {
+        override fun onStart() {
+            super.onStart()
+            setContentView(R.layout.activity_main)
+            Toast.makeText(this, "Activity Started", Toast.LENGTH_LONG)
+                .show()
+
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Activity Resumed", Toast.LENGTH_LONG)
+            .show()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Activity Stopped", Toast.LENGTH_LONG)
+            .show()
+    }
+
+
+
+}
+
+
